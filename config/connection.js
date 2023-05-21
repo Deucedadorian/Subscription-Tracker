3d6047f3cd9300d17894cbf579e4eb6caf9e11f8
Deucedadorian <logboarddjc@gmail.com>
2023-05-20 23:25:08 -0400

    update npms

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,24 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'subscription-manager-db.cec8wmeazaqa.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    logging: console.log,
+    maxConcurrentQueries: 100,
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl:'Amazon RDS'
+    },
+    pool: { maxConnections: 5, maxIdleTime: 30},
+    language: 'en'
+  }
+);
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+console.log(sequelize);
 
 module.exports = sequelize;
