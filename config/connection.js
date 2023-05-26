b1ef6ac64a9ebbab2b5ecacf5b6351d3565197ae
Deucedadorian <logboarddjc@gmail.com>
2023-05-26 19:25:49 -0400

    implement database on AWS

--- config/connection.js
+++ config/connection.js
@@ -6,19 +6,17 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'subscription-manager-db.cec8wmeazaqa.us-east-1.rds.amazonaws.com',
-    port: 3306,
+    host: process.env.DB_HOSTNAME,
+    dialect: "mysql",
+    port: process.env.DB_PORT,
     logging: console.log,
     maxConcurrentQueries: 100,
-    dialect: 'mysql',
     dialectOptions: {
-      ssl:'Amazon RDS'
+      ssl:'Amazon RDS',
     },
     pool: { maxConnections: 5, maxIdleTime: 30},
     language: 'en'
   }
 );
-
-console.log(sequelize);
 
 module.exports = sequelize;
